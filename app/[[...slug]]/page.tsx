d7e7dd0443ae6581021011788c206cc7a128f7ad
Jason Karlavige <jkarlavige@gmail.com>
2023-11-03 11:08:06 -0400

    add test optional catch-all route

---
+++ app/[[...slug]]/page.tsx
@@ -0,0 +1,24 @@
+export const dynamicParams = false
+
+const Test = ({ params }) => {
+  console.log('params', params)
+  return (
+    <>
+      <h1>Slug</h1>
+    </>
+  )
+}
+
+export async function generateStaticParams() {
+  // Endpoint that would only return slugs of every page builder page from Sanity
+
+  // This correctly loads /page and 404s all other routes
+  const allPages = [{ slug: ['page'] }]
+
+  // This causes all routes to load an empty page and not 404
+  // const allPages = []
+
+  return allPages
+}
+
+export default Test
